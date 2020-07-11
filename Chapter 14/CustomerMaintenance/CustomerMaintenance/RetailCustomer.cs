349b17f4414709fe6f5cce22696d9676dc2f7a48
Joseph Herrera <josephherreraleon@outlook.com>
2020-07-11 18:07:13 -0500

    Chapter 14

---
+++ Chapter 14/CustomerMaintenance/CustomerMaintenance/RetailCustomer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerMaintenance
+{
+	class RetailCustomer : Customer
+	{
+		private string homePhone;
+
+		public string HomePhone
+		{
+			get { return homePhone; }
+			set { homePhone = value; }
+		}
+
+		public RetailCustomer() { }
+
+		public RetailCustomer(string firstName, string lastName, string email, string homePhone)
+		{
+			base.FirstName = firstName;
+			base.LastName = lastName;
+			base.Email = email;
+			HomePhone = homePhone;
+		}
+
+		public override string GetDisplayText()
+		{
+			return base.GetDisplayText() + " ph:" + HomePhone;
+		}
+
+	}
+}
---
+++ Chapter 14/CustomerMaintenance/CustomerMaintenance/WholesaleCustomer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerMaintenance
+{
+	class WholesaleCustomer : Customer
+	{
+		private string company;
+
+		public WholesaleCustomer()
+		{
+
+		}
+
+		public WholesaleCustomer(string firstName, string lastName, string email, string company)
+		{
+			base.FirstName = firstName;
+			base.LastName = lastName;
+			base.Email = email;
+			Company = company;
+		}
+
+		public string Company
+		{
+			get { return company; }
+			set { company = value; }
+		}
+
+		public override string GetDisplayText()
+		{
+			return base.GetDisplayText() + "(" + Company + ")";
+		}
+
+	}
+}
